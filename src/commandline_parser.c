eb20f85a1fc36980e08e179a0e44f2491de60eba
Henok Jackson <henokjackson@gmail.com>
2026-05-23 00:52:18 +0530

    Added commandline parser logic

--- src/commandline_parser.c
+++ src/commandline_parser.c
@@ -5,8 +5,47 @@
  */
 
 #include "../include/commandline_parser.h"
+#include <stdio.h>
+#include <string.h>
 
+#define MAX_COMMAND_LINE_ARGS_COUNT 10
+#define HEX_VIEW_ARG_STR "--hexview"
 
-void extract_commandline_arguments(int argc, char** argv, char** args) {
-    // Iterate over the array of commandline args
+bool extern g_hex_view = false;
+bool extern g_file_path_set = false;
+char extern g_file_path[500];
+
+void show_commandline_help() {
+    printf("Invalid arguments !");
+    printf("\n\nUsage: esp8266-multitool [FLAGS ...]\n");
+    printf("Example:\n");
+    printf("\tToggling hex view:\n");
+    printf("\t\t--hexview filename\tGenerates the maximum possible fibonacci number that can be generated with the given heap.\n");
+}
+
+void extract_commandline_arguments(const int argc, const char** argv) {
+    if (argc > MAX_COMMAND_LINE_ARGS_COUNT) {
+        printf("Too many arguments !\n");
+        return;
+    }
+
+    for (int i = 0; i < argc; i++) {
+        bool command_invalid = true;
+
+        if (g_hex_view == false && strcmp(HEX_VIEW_ARG_STR, argv[i]) == 0) {
+            g_hex_view = true;
+            command_invalid = false;
+        }
+
+        if (g_file_path_set == false &&
+            argv[i][0] != '-' &&
+            argv[i][1] != '-') {
+            strcpy(g_file_path, argv[i]);
+        }
+
+        if (command_invalid == true) {
+            show_commandline_help();
+            return;
+        }
+    }
 }
